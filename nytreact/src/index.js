66bf81b2d8268fecb9010e16ef7d523de5b1bc20
Dustin J Siegel <weenzombie@gmail.com>
2017-09-30 01:05:41 -0600

    added constructor files

--- nytreact/src/index.js
+++ nytreact/src/index.js
@@ -1,8 +1,16 @@
+//Note: This is a Componant(...something that ends up on the screen), REMEMBER: Only one componant per file!
+
+// Calls the react and react-dom files from the package.json to grab componants to nest and render them.
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// A variable to represent the youtube api key (may be the wrong key, if so, rewatch YOuTube Searcj API SignUp)
+const API_KEY = '';
+
+// Create a new componants. A componant or view should produce html.
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+// Take this componants generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
